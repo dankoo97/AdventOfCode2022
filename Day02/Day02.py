4850e95780961eab2df361990e6bc560ca390193
dankoo97 <dak.7997@gmail.com>
2022-12-02 00:20:58 -0500

    Day02 complete

--- Day02/Day02.py
+++ Day02/Day02.py
@@ -6,16 +6,52 @@ class Day02 (AdventOfCodeBase.AoCProblem):
         super().__init__(myInput)
 
     def readInput(self, myInput):
-        pass
+        with open(myInput, 'r') as myFile:
+            gameRound = []
+            for line in myFile.read().strip().split('\n'):
+                gameRound.append(line.split())
+        return gameRound
 
     def parseInput(self, myInput):
-        pass
+        return myInput
 
     def p1(self):
-        pass
+        score = 0
+        points = {
+            'X': 1,
+            'Y': 2,
+            'Z': 3,
+        }
+        for them, me in self.values:
+            score += points[me]
+            them = chr(ord(them) + 23)
+            if them == me:
+                score += 3
+            elif chr((ord(them) + 1) % 3 + 88) == me:
+                score += 0
+            else:
+                score += 6
+        return score
 
     def p2(self):
-        pass
+        score = 0
+        points = {
+            'A': 1,
+            'B': 2,
+            'C': 3,
+            'X': 0,
+            'Y': 3,
+            'Z': 6,
+        }
+        for them, me in self.values:
+            score += points[me]
+            if me == 'X':
+                score += points[chr((ord(them)) % 3 + 65)]
+            elif me == 'Z':
+                score += points[chr((ord(them) - 1) % 3 + 65)]
+            else:
+                score += points[them]
+        return score
 
 
 AdventOfCodeBase.run(Day02, "input.txt")
