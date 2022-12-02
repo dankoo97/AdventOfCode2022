953a900e5bf1b79a5a74408767a1b33783b8edac
dankoo97 <dak.7997@gmail.com>
2022-12-01 23:58:16 -0500

    Pre Day02 setup

---
+++ Day02/Day02.py
@@ -0,0 +1,21 @@
+import AdventOfCodeBase
+
+
+class Day02 (AdventOfCodeBase.AoCProblem):
+    def __init__(self, myInput):
+        super().__init__(myInput)
+
+    def readInput(self, myInput):
+        pass
+
+    def parseInput(self, myInput):
+        pass
+
+    def p1(self):
+        pass
+
+    def p2(self):
+        pass
+
+
+AdventOfCodeBase.run(Day02, "input.txt")
