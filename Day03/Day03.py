6b55657d7c234606d87e275d0a61996da389cd4c
dankoo97 <dak.7997@gmail.com>
2022-12-03 09:39:50 -0500

    Improved Day03 algorithm to memoize priority and better parse input

--- Day03/Day03.py
+++ Day03/Day03.py
@@ -4,6 +4,7 @@ import AdventOfCodeBase
 class Day03 (AdventOfCodeBase.AoCProblem):
     def __init__(self, myInput):
         super().__init__(myInput)
+        self.map = {c: i for i, c in enumerate('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 1)}
 
     def readInput(self, myInput):
         with open(myInput, 'r') as myFile:
@@ -13,24 +14,22 @@ class Day03 (AdventOfCodeBase.AoCProblem):
             return result
 
     def parseInput(self, myInput):
-        result = []
-        for line in myInput:
-            result.append((line[:len(line) >> 1], line[len(line) >> 1:]))
-        return result
+        return myInput
 
     def p1(self):
         sum = 0
-        for a, b in self.values:
+        for line in self.values:
+            a, b = line[:len(line) >> 1], line[len(line) >> 1:]
             c = set(a) & set(b)
-            sum += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.index(c.pop()) + 1
+            sum += self.map[c.pop()]
         return sum
 
     def p2(self):
         sum = 0
         for i in range(0, len(self.values), 3):
             a, b, c = self.values[i:i+3]
-            r = {*a[0], *a[1]} & {*b[0], *b[1]} & {*c[0], *c[1]}
-            sum += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.index(r.pop()) + 1
+            r = set(a) & set(b) & set(c)
+            sum += self.map[r.pop()]
         return sum
 
 
