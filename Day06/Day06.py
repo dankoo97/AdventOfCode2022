16d7ef969e0b11e0cce99bfad532b71dcb319dc0
dankoo97 <dak.7997@gmail.com>
2022-12-06 00:05:12 -0500

    Day06 answer

---
+++ Day06/Day06.py
@@ -0,0 +1,25 @@
+import AdventOfCodeBase
+
+
+class Day06(AdventOfCodeBase.AoCProblem):
+    def __init__(self, myInput):
+        super().__init__(myInput)
+
+    def readInput(self, myInput):
+        with open(myInput, 'r') as myFile:
+            return myFile.read().strip()
+
+    def p1(self):
+        s = [c for c in self.values]
+        for i, c in enumerate(s, 4):
+            if len(set(s[i-4:i])) == 4:
+                return i
+
+    def p2(self):
+        s = [c for c in self.values]
+        for i, c in enumerate(s, 14):
+            if len(set(s[i - 14:i])) == 14:
+                return i
+
+
+AdventOfCodeBase.run(Day06, "input.txt")
